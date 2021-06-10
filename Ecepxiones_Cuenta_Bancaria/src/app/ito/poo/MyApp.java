56217131c7ec1a5de164676555d7174005c20e31
Lupita <Lupita@Lupita>
2021-06-09 22:50:42 -0500

    Archivos binarios y de texto

---
+++ Ecepxiones_Cuenta_Bancaria/src/app/ito/poo/MyApp.java
@@ -0,0 +1,9 @@
+package app.ito.poo;
+import ecepxiones.ito.poo.*;
+
+public class MyApp {
+	public static void main(String[] args) throws  CuentaException, EliminaException, RetiroException, ExcepcionRetiro {	
+		Aplicacion.run();
+		
+	}
+}
---
+++ Ecepxiones_Cuenta_Bancaria/src/app/ito/poo/MyApp2.java
@@ -0,0 +1,14 @@
+package app.ito.poo;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import ecepxiones.ito.poo.*;
+
+
+public class MyApp2{
+	public static void main(String[] args) throws  CuentaException, EliminaException, RetiroException, ExcepcionRetiro, IOException{
+		Aplicacion2.menuoperac();
+		
+	}
+}
